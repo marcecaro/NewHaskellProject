6932f22a4a615122c4712a8e1fe98bc49456fae4
marcecaro <marcecaro@gmail.com>
2022-09-19 18:44:49 +0000

    Adding

--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,20 @@
 module Main where
-import           Module.RpnSafe
+
+import           Control.Monad.RWS.Strict
 import           Control.Monad.State
+import           Module.AppTypes
+import           Module.MonadRWST
+import           Module.RpnSafe
+
 -- []
 -- (,,)
 -- data  Maybe a  =  Nothing | Just a
 -- data  Either a b  =  Left a | Right b
 -- data NonEmpty a = a :| [a]
 -- IO
+runMyApp :: MyApp logEntry state a -> AppConfig -> state -> IO (a, [logEntry])
+runMyApp app config st = evalRWST app (initialEnv config) st
+
 main = do
   let res = runStateT calculate []
   print res
