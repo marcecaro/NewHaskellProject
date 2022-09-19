937708d0b9da31997f5685a6a2ed9b706c2959ad
marcecaro <marcecaro@gmail.com>
2022-09-19 01:11:17 +0000

    Finished with workspaces recomendation plugins

--- Learning-Samples/Samples/Monads.Reader.hs
+++ Learning-Samples/Samples/Monads.Reader.hs
@@ -1,37 +1,36 @@
- -- {-# LANGUAGE FlexibleContexts #-}
 module Samples.Monads.Reader where
-import Control.Monad.Reader
 
+import           Control.Monad.Reader
 
 -- Environment that keeps global values
-newtype Environment = Environment { getLogLevel::String }  deriving Show
-
+newtype Environment =
+  Environment
+    { getLogLevel :: String
+    }
+  deriving (Show)
 
 -- Create a default environment
 getDefaultEnv :: Environment
 getDefaultEnv = Environment "DEBUG"
 
-
 -- Function that logs but adding a levelq
-logger:: String -> String-> IO ()
-logger level mesg =  print $ level ++": "++mesg
-
+logger :: String -> String -> IO ()
+logger level mesg = print $ level ++ ": " ++ mesg
 
 --bussinesLogic::  (MonadReader Environment m, MonadIO m)  => m ()
 -- Bussines logic that will be runned by runReaderT, and do IO opperations
-bussinesLogic = do 
-                    env <- asks getLogLevel
-                    liftIO (logger env "Starting Application")     -- liftIO for using IO inside a reader
-                    local (\env -> Environment "INFO")$ do                          -- Change a local environment
-                                                            env <- asks getLogLevel
-                                                            liftIO (logger env "Running in info <<---")
-                    liftIO $ logger env "Getting back on previous environment"
-                    return ()
+bussinesLogic = do
+  env <- asks getLogLevel
+  liftIO (logger env "Starting Application") -- liftIO for using IO inside a reader
+  local (\env -> Environment "INFO") $ -- Change a local environment
+   do
+    env <- asks getLogLevel
+    liftIO (logger env "Running in info <<---")
+  liftIO $ logger env "Getting back on previous environment"
+  return ()
 
-
-main:: IO()
-main = do 
-            let env = getDefaultEnv
-            runReaderT bussinesLogic  env
-            return ()
-    
+main :: IO ()
+main = do
+  let env = getDefaultEnv
+  runReaderT bussinesLogic env
+  return ()
